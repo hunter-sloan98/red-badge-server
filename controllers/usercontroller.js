d7c33925663ff157f5a108eae24a23edfc22b0b0
hunter-sloan98 <sloan.hunter98@gmail.com>
2021-09-23 18:38:54 -0400

    Signup, Login & Myprofile Endpoints

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const { models } = require('../models');
+// const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UniqueConstraintError } = require('sequelize/lib/errors');
+const User = require('../models/user');
 
+//*Signup
 router.post('/signup', async (req, res) => {
   const { username, password } = req.body.user;
   try {
@@ -75,5 +78,57 @@ router.post('/login', async (req, res) => {
     })
   }
 })
+
+//*Show user info
+//TODO: This works as intended: Need to make it grab the current user id, for now it has to be entered manually
+//?Credit for this code format goes to Lars Blide on youtube. Changes were made to fit this code but his rough outline was insturmental
+router.get('/myprofile/:id', async (req, res) => {
+  User.findByPk(req.params.id)
+  .then(userFound => {
+    if (!userFound) { return res.status(404).end(); }
+    return res.status(200).json(userFound);
+  })
+  .catch( err => next(err));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//*This one does display all users in postman: So slight success!!
+// router.get('/myprofile', async (req, res) => {
+//   let { id } = req.body;
+//   try{
+//     const myProfile = await models.UserModel.findAll();
+//     res.status(200).json(myProfile);
+//   } catch(err) {
+//     res.status(500).json({
+//       error: `${err}`
+//     })
+//   }
+// })
+//*Orignal code: Getting an undefined for the id 
+// router.get('/myprofile', async (req, res) => {
+//   let { id } = req.user;
+//   try{
+//     const myProfile = await models.UserModel.findOne({
+//       where: {
+//         id,
+//       }
+//     });
+//     res.status(200).json(myProfile);
+//   } catch(err) {
+//     res.status(500).json({
+//       error: `${err}`
+//     })
+//   }
+// })
 
 module.exports = router;
